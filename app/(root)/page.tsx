d2f34f712a41af2f55867148fc83e009e0442ac0
iancarr1412 <iancarr1412@live.co.uk>
2023-10-12 18:59:22 +0100

    1st commit - shadcn.ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <h1>Admin Dashboard</h1>
+      <Button size="default" variant="default" >Click Here!</Button>
+    </div>
+  )
+}
