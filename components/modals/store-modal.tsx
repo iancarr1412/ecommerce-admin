b6ebb1121f1e7d46da759f206a018079b882d50c
iancarr1412 <iancarr1412@live.co.uk>
2023-10-17 19:09:51 +0100

    Planetscale connection

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,7 +3,7 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormField, FormItem, FormLabel, FormControl } from "@/components/ui/form";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -31,33 +31,29 @@ export const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}>
             <div className="space-y-4 py-2 pb-4">
-                <Form {form...}> 
-                
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Store Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="ECommerce" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                    />
+                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                            <Button type="submit">Continue</Button>
+                        </div>
+                    </form>
                 </Form>
             </div>
 
         </Modal>
     );
 };
-
-/*
-    <Form {form...}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                            <Input placeholder="ECommerce" {...field} />
-                        </FormControl>
-                    </FormItem>
-                )}
-            />
-            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button>Cancel</Button>
-                <Button>Continue</Button>
-            </div>
-        </form>
-*/
